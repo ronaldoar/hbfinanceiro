a69775d708c3bad6b890536e17ef96be167eea3b
= <ronaldo.alves.rios@gmail.com>
2022-03-25 14:53:48 -0300

    Artefatos iniciais do projeto

---
+++ src/main/java/br/com/tcs/hbfinanceiro/HbeerFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcs.hbfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbeerFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbeerFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tcs/hbfinanceiro/HbeerFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcs.hbfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbeerFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
